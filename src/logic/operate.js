d7ef70e7011b56762b5d3ef205d503b911610c84
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2022-12-10 00:51:12 +0300

    Add functionality

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -9,7 +9,7 @@ export default function operate(numberOne, numberTwo, operation) {
   if (operation === '-') {
     return one.minus(two).toString();
   }
-  if (operation === 'x') {
+  if (operation === '×') {
     return one.times(two).toString();
   }
   if (operation === '÷') {
@@ -20,11 +20,7 @@ export default function operate(numberOne, numberTwo, operation) {
     }
   }
   if (operation === '%') {
-    try {
-      return one.mod(two).toString();
-    } catch (err) {
-      return "Can't find modulo as can't divide by 0.";
-    }
+    return one.mod(two).toString();
   }
   throw Error(`Unknown operation '${operation}'`);
 }
