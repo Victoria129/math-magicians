71e4443d7d14d2076588cbc767632fc80854a451
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2022-12-24 00:58:33 +0300

    Add home page

---
+++ src/components/Quote.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import conCSS from './Quote.module.css';
+
+const Quote = () => (
+  <div className={conCSS.container}>
+    <h1>Quote</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Blanditiis optio porro consequatur, ratione ipsam, perspiciatis
+      enim officiis earum sit vero aperiam iste! Et beatae cumque expedita odit!
+    </p>
+  </div>
+);
+
+export default Quote;
