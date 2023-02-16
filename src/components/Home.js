1e8d6438fdcdc3ddf40227919f377b2a99d2a83b
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-02-16 13:46:27 +0300

    Add events to the project

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ const Home = () => (
   <div className={conCSS.container}>
     <h1>Home Page</h1>
     <p>
-    Great minds think big. Giants in science create
-    sensible values that bring light to the most obscure things!
+      Great minds think big. Giants in science create
+      sensible values that bring light to the most obscure things!
     </p>
   </div>
 
